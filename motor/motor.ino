edfdee9c00a990250366f9c68701d55a00600475
karol <karol.misiarz@robovision.eu>
2023-08-04 13:09:17 +0200

    Add class Motor

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,7 +1,6 @@
 #include <IoAbstraction.h>
 #include <ExecWithParameter.h>
 
-
 #define EN_1 5
 #define DIR_1 6
 #define PUL_1 7
@@ -35,8 +34,9 @@ void engine_direction(bool forward){
   moving_forward = forward;
 }
 
-class Motor: public Executable {
+class Motor : public Executable {
 private:
+  int id;
   int enable_pin;
   int direction_pin;
   int pulse_pin;
@@ -47,109 +47,125 @@ private:
   bool moving_forward;
 
 public:
-  Motor(int enable_pin, int direction_pin, int pulse_pin, TaskManager* tm){
-    enable_pin = enable_pin;
-    direction_pin = direction_pin;
-    pulse_pin = pulse_pin;
-    taskManager = tm;
+  Motor(int id, int enable_pin, int direction_pin, int pulse_pin, TaskManager* tm){
+    this->id = id;
+    this->enable_pin = enable_pin;
+    this->direction_pin = direction_pin;
+    this->pulse_pin = pulse_pin;
+    this->taskManager = tm;
   }
 
   void enable(bool enabled){
-    digitalWrite(enable_pin, enabled? LOW : HIGH);
+    digitalWrite(this->enable_pin, enabled? LOW : HIGH);
   }
   void set_direction(bool forward) {
-    digitalWrite(direction_pin, forward? HIGH : LOW);
+    digitalWrite(this->direction_pin, forward? HIGH : LOW);
   }
   
   void update_frequency_target(int new_frequency_target){
-    frequency_target = new_frequency_target;
+    this->frequency_target = new_frequency_target;
   }
 
-  // void update_frequency_and_move(int frequency)
+  // Function to make a step with the specified delay
+  void make_step(int delay) {
+    digitalWrite(this->pulse_pin, HIGH);
+    // Capture the 'this' pointer in the lambda function
+    auto task = new ExecWith2Parameters<int, bool>(digitalWrite, this->pulse_pin, LOW);
 
+    taskManager->schedule(onceMicros(delay), task);
+    // taskManager->schedule(
+    //   onceMicros(delay), [this](){
+    //     digitalWrite(this->pulse_pin, LOW);
+    // }
+    // );
+  }
+
+  void exec() override {
+    x_val = analogRead(JOYSTICK);
+    int x_dir = x_val - 507;
+
+    Serial.print("Motor ");
+    Serial.print(id);
+    Serial.print(": x: ");
+    Serial.print(x_dir);
+
+    // joystick moved, update target dir
+    // TODO change this to update_frequency_target for each motor
+    this->frequency_target = x_dir * MAX_SPEED;
+    
+    Serial.print(", freq: ");
+    Serial.print(frequency);
+
+    Serial.print(", freq_target: ");
+    Serial.println(frequency_target);
+
+    // joystick moved when engine was off
+    if (abs(x_dir) > JOYSTICK_CUT_OFF && !this->enabled) {
+
+      Serial.print("\nMOTOR ");
+      Serial.print(id);
+      Serial.println(" STARTING ");
+      // set the starting frequency
+      this->frequency = (x_dir > 0) ? FREQ_CUT_OFF : -FREQ_CUT_OFF;
+      // auto go_left = new ExecWithParameter<int>(update_frequency_and_move, 0);
+      // auto go_right = new ExecWithParameter<int>(update_frequency_and_move, 1);
+      update_frequency_and_move();
+      // taskManager.schedule(0, go_right);
+    }
+  }
+
+  void update_frequency_and_move(){
+    int frequency_diff = this->frequency - this->frequency_target;
+    if (abs(frequency_diff) >= FREQ_ACCELERATION) {
+      if (frequency_diff > 0)
+        frequency = max(this->frequency - FREQ_ACCELERATION, this->frequency_target);
+      else 
+        frequency = min(this->frequency + FREQ_ACCELERATION, this->frequency_target);
+    }
+    // Serial.print("frequency: ");
+    // Serial.print(frequency);
+    // stop motor if current frequency is around 0
+    if (abs(this->frequency) <= FREQ_CUT_OFF){
+      Serial.print("\nMOTOR ");
+      Serial.print(id);
+      Serial.println(" FREQ LESS THAN CUT OFF ");
+      enable(false);
+      return;
+    }
+
+    // motor will move, enable it if not yet
+    if (!this->enabled)
+      Serial.print("\nMOTOR ");
+      Serial.print(id);
+      Serial.println(" ENABLING ENGINE");
+      enable(true);
+
+    // update direction based on frequency sign
+    if (this->frequency > 0 && !this->moving_forward){
+      Serial.print("\nMOTOR ");
+      Serial.print(id);
+      Serial.println(" CHANGING DIRECTION -> FORWARD");
+      set_direction(true);
+    }
+    else if (this->frequency < 0 && this->moving_forward){
+      Serial.print("\nMOTOR ");
+      Serial.print(id);
+      Serial.println(" CHANGING DIRECTION -> BACK");
+      set_direction(false);
+    }
+    
+    // set delay between the pulses
+    int delay_us = 1000000 / abs(this->frequency);
+    make_step(int(delay_us/2));
+    // auto task = new ExecWithParameter<bool>(update_frequency_and_move, false);
+
+    this->taskManager->schedule(onceMicros(delay_us), [this](){
+      update_frequency_and_move();
+    });
+    // this->taskManager->schedule(onceMicros(delay_us), update_frequency_and_move);
+  }
 };
 
-void update_frequency_and_move(){
-  // update frequency according to the current and target
-  int frequency_diff = frequency - frequency_target;
-  if (abs(frequency_diff) >= FREQ_ACCELERATION) {
-    if (frequency_diff > 0)
-      frequency = max(frequency - FREQ_ACCELERATION, frequency_target);
-    else 
-      frequency = min(frequency + FREQ_ACCELERATION, frequency_target);
-  }
-  // Serial.print("frequency: ");
-  // Serial.print(frequency);
-  // stop motor if current frequency is around 0
-  if (abs(frequency) <= FREQ_CUT_OFF){
-    Serial.println("\n FREQ LESS THAN CUT OFF");
-    engine_enable(false);
-    return;
-  }
-
-  // motor will move, enable it if not yet
-  if (!engine_enabled)
-    Serial.println("\n ENABLING ENGINE");
-    engine_enable(true);
-
-  // update direction based on frequency sign
-  if (frequency > 0 && !moving_forward){
-    Serial.println("\n CHANGING DIRECTION -> FORWARD");
-    engine_direction(true);
-  }
-  else if (frequency < 0 && moving_forward){
-    Serial.println("\n CHANGING DIRECTION -> BACK");
-    engine_direction(false);
-  }
-  
-  // set delay between the pulses
-  delay_us = 1000000 / abs(frequency);
-
-
-  // make a step
-  digitalWrite(PUL_1, HIGH);  
-  digitalWrite(PUL_2, HIGH);  
-  taskManager.schedule(onceMicros(int(delay_us/2)), [] {
-      digitalWrite(PUL_1, LOW);   
-      digitalWrite(PUL_2, LOW);   
-    }
-  );
-
-  // run the same task with the same parameter again
-  // auto task = new ExecWithParameter<int>(update_frequency_and_move, motor_id);
-  taskManager.schedule(onceMicros(delay_us), update_frequency_and_move);
-}
-
-void read_joystick(){
-  x_val = analogRead(JOYSTICK);
-  x_dir = x_val - 507;
-
-  Serial.print(" x: ");
-  Serial.print(x_dir);
-
-  // joystick moved, update target dir
-  // TODO change this to update_frequency_target for each motor
-  frequency_target = x_dir * MAX_SPEED;
-
-  
-  Serial.print(", freq: ");
-  Serial.print(frequency);
-
-  Serial.print(", freq_target: ");
-  Serial.println(frequency_target);
-
-  // joystick moved when engine was off
-  if (abs(x_dir) > JOYSTICK_CUT_OFF && !engine_enabled) {
-
-    Serial.println(" STARTING ");
-    // set the starting frequency
-    frequency = (x_dir > 0) ? FREQ_CUT_OFF : -FREQ_CUT_OFF;
-    // auto go_left = new ExecWithParameter<int>(update_frequency_and_move, 0);
-    // auto go_right = new ExecWithParameter<int>(update_frequency_and_move, 1);
-    update_frequency_and_move();
-    // taskManager.schedule(0, go_right);
-  }
-}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -162,27 +178,16 @@ void setup() {
   pinMode(PUL_2, OUTPUT);
 
   Serial.begin(9600);
-  engine_enable(false);
-  engine_direction(true);
+  // engine_enable(false);
+  // engine_direction(true);
 
-  taskManager.scheduleFixedRate(100, read_joystick);
+  Motor motor1(0, EN_1, DIR_1, PUL_1, &taskManager);
+  motor1.enable(false);
+  motor1.set_direction(true);
+
+  taskManager.scheduleFixedRate(100, &motor1);
 }
 
 void loop() {
-
   taskManager.runLoop();
-
-  
-  // Serial.print("x: ");
-  // Serial.print(x_dir);
-  // Serial.print(", freq_target: ");
-  // Serial.print(frequency_target);
-  // Serial.print(", freq: ");
-  // Serial.print(frequency);
-  // Serial.print(", freq_diff: ");
-  // Serial.print(frequency_diff);
-  // Serial.print(", delay: ");
-  // Serial.print(delay_us);
-  // Serial.println("");
-
 }
